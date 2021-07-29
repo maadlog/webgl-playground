6e44605d4a4e810fd135a9719a0c2ee35a6ec60c
Martin Loguancio <martin.loguancio@gmail.com>
2021-07-29 18:39:16 -0300

    Removed more duplication

--- Objects/GameObject.Wireframe.js
+++ Objects/GameObject.Wireframe.js
@@ -1,5 +1,5 @@
 class Wireframe extends GameObject {
-	
+
   initBuffers(ctx,vertices,indices) {
     const positionBuffer = ctx.createBuffer();
 
@@ -22,11 +22,11 @@ class Wireframe extends GameObject {
     }
 
     const colorBuffer = ctx.createBuffer();
-    
+
     ctx.bindBuffer(ctx.ARRAY_BUFFER, colorBuffer);
 
     ctx.bufferData(ctx.ARRAY_BUFFER, new Float32Array(colorsArray), ctx.STATIC_DRAW);
-    
+
     this.colors = colorBuffer;
 
     const indexBuffer = ctx.createBuffer();
@@ -56,49 +56,8 @@ class Wireframe extends GameObject {
   };
 
   render(ctx,viewMatrix,projectionMatrix) {
-	
-    this.material.renderBind(ctx,this.transformMatrix,viewMatrix,projectionMatrix);
-
-
-	    const numComponents = 3;  // pull out 2 values per iteration
-	    const type = ctx.FLOAT;    // the data in the buffer is 32bit floats
-	    const normalize = false;  // don't normalize
-	    const stride = 0;         // how many bytes to get from one set of values to the next
-	                 				// 0 = use type and numComponents above
-	    const offset = 0;         // how many bytes inside the buffer to start from
-
-		ctx.bindBuffer(ctx.ARRAY_BUFFER, this.position);
-	    ctx.vertexAttribPointer(
-	        this.material.programInfo.attribLocations.vertexPosition,
-	        numComponents,
-	        type,
-	        normalize,
-	        stride,
-	        offset);
-
-	    ctx.enableVertexAttribArray(
-	        this.material.programInfo.attribLocations.vertexPosition);
-	     
-	    const numComponentsColor = 4;  // pull out 2 values per iteration
-	 
-		ctx.bindBuffer(ctx.ARRAY_BUFFER, this.colors);
-	    ctx.vertexAttribPointer(
-	        this.material.programInfo.attribLocations.vertexColor,
-	        numComponentsColor,
-	        type,
-	        normalize,
-	        stride,
-	        offset);		
-
-	    ctx.enableVertexAttribArray(
-        	this.material.programInfo.attribLocations.vertexColor);
-	     
-		
-		ctx.bindBuffer(ctx.ELEMENT_ARRAY_BUFFER, this.indices);
-
-		    const vertexCount = this.indices_count;
-		    const type2 = ctx.UNSIGNED_SHORT;
-		    ctx.drawElements(ctx.LINES, vertexCount, type2, offset);
+      super.render(ctx, viewMatrix, projectionMatrix)
+      ctx.drawElements(ctx.LINES, this.indices_count, ctx.UNSIGNED_SHORT, 0);
   };
 
   updateMatrix(transform) {
